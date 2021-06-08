8d51b024dc25f1b60c9986fd53a19f89934f976a
ernikus <ernikus2000@gmail.com>
2021-06-08 16:39:49 +0200

    Update Database
    Update Database

---
+++ Programs/Cat80Times/main.cpp
@@ -0,0 +1,22 @@
+﻿//Program:      Cat80Times
+//Soln Author:  ernikus
+
+#include <iostream>
+
+void printWord(std::string word)
+{
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			std::cout << word << " ";
+		}
+
+		std::cout << std::endl;
+	}
+}
+
+int main()
+{
+	printWord("cat");
+}
---
+++ Programs/DivideBy5/main.cpp
@@ -0,0 +1,17 @@
+﻿//Program:      DivideBy5
+//Soln Author:  ernikus
+
+#include <iostream>
+
+void print5(int number)
+{
+	for (int i = number; i > 0; i -= 5)
+	{
+		std::cout << i << " ";
+	}
+}
+
+int main()
+{
+	print5(555);
+}
