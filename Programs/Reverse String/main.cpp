15214d54edc68bc8ef4ae4c4703d212fdf01012a
ernikus <ernikus2000@gmail.com>
2021-07-08 16:40:59 +0200

    Adding Reverse-String Program

---
+++ Programs/Reverse String/main.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+std::string reverse(std::string input)
+{
+    std::string x{};
+    
+    for (int loop_i = input.size()-1; loop_i >= 0; loop_i--)
+    {
+        x += input[loop_i];
+    }
+    
+    return x;
+}
+
+int main()
+{
+    std::string str = "Testowy";
+
+    std::string rev_str = reverse(str);
+
+    std::cout << rev_str << std::endl;
+
+    return 0;
+}
